e246cf8b59686a37c309126ec8b8cc398586139b
Martin Frigaard <mjfrigaard@gmail.com>
2019-09-11 11:58:19 -0700

    New linear regression images

--- code/17.1-import.R
+++ code/17.1-import.R
@@ -30,7 +30,7 @@ utils::download.file(
   destfile = paste0(
     "data/",
     base::noquote(lubridate::today()),
-    "-amy-schumer.csv"
+    "raw-amy-schumer.csv"
   )
 )
 # download amy schumer
@@ -40,7 +40,7 @@ utils::download.file(
   destfile = paste0(
     "data/",
     base::noquote(lubridate::today()),
-    "-key-and-peele.csv"
+    "raw-key-and-peele.csv"
   )
 )
 
@@ -48,11 +48,54 @@ utils::download.file(
 AmySchumerRaw <- readr::read_csv(paste0(
   "data/",
   base::noquote(lubridate::today()),
-  "-amy-schumer.csv"
+  "raw-amy-schumer.csv"
 ))
 
 KeyAndPeeleRaw <- readr::read_csv(paste0(
   "data/",
   base::noquote(lubridate::today()),
-  "-key-and-peele.csv"
+  "raw-key-and-peele.csv"
 ))
+
+
+# create simulated data  --------------------------------------------------
+# from this post
+# https://www.r-bloggers.com/simulating-random-multivariate-correlated-data-continuous-variables/
+# 
+R <- matrix(cbind(1,.80,.2,  
+                  .80,1,.7,  
+                  .2,.7, 1), nrow = 3)
+U <- t(chol(R))
+# number of variables 
+nvars <- dim(U)[1]
+# create the number of observations
+numobs <- 100000
+# set random seed
+set.seed(20)
+
+random.normal <- matrix(data = rnorm(nvars * numobs, 2, 4), 
+                        nrow = nvars, 
+                        ncol = numobs)
+# create X 
+X <- U %*% 
+    random.normal
+# t distribution
+newX <- t(X)
+
+# put in data frame
+raw <- as.data.frame(newX)
+
+orig.raw <- as.data.frame(t(random.normal))
+
+names(raw) <- c("outcome", "predictor_1", "predictor_2")
+
+SimDat <- raw %>% 
+    dplyr::sample_n(size = 10000) %>% 
+    dplyr::mutate(predictor_2 = abs(predictor_2*2))
+    
+# remove all previous junk
+rm(R, U, nvars, numobs, random.normal, X, newX, raw, orig.raw)
+
+# cor(raw)
+# cor(orig.raw)
+
